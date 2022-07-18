c52b1e3e7eb727325d2a9ca52823ecd1a635f950
Stefan Utamaru <stefafafan@hatena.ne.jp>
2022-07-18 11:44:07 +0900

    add spacing

--- golang/app.go
+++ golang/app.go
@@ -389,9 +389,9 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	err := db.Select(&results,
 		"SELECT posts.id, posts.user_id, posts.body, posts.mime, posts.created_at FROM posts"+
-			"JOIN users ON users.id = posts.user_id"+
-			"WHERE users.del_flg = 0"+
-			"ORDER BY posts.created_at DESC LIMIT 20")
+			" JOIN users ON users.id = posts.user_id"+
+			" WHERE users.del_flg = 0"+
+			" ORDER BY posts.created_at DESC LIMIT 20")
 	if err != nil {
 		log.Print(err)
 		return

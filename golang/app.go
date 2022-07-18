ad29f5b8bde0ed5cb194d240586dcc9695e0c38f
Stefan Utamaru <stefafafan@hatena.ne.jp>
2022-07-18 19:50:22 +0900

    os.execで外部コマンド実行をやめる

--- golang/app.go
+++ golang/app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	crand "crypto/rand"
+	"crypto/sha512"
+	"encoding/hex"
 	"fmt"
 	"html/template"
 	"io"
@@ -10,7 +12,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"path"
 	"regexp"
 	"strconv"
@@ -117,14 +118,9 @@ func escapeshellarg(arg string) string {
 }
 
 func digest(src string) string {
-	// opensslのバージョンによっては (stdin)= というのがつくので取る
-	out, err := exec.Command("/bin/bash", "-c", `printf "%s" `+escapeshellarg(src)+` | openssl dgst -sha512 | sed 's/^.*= //'`).Output()
-	if err != nil {
-		log.Print(err)
-		return ""
-	}
-
-	return strings.TrimSuffix(string(out), "\n")
+	s := sha512.New()
+	s.Write([]byte(src))
+	return hex.EncodeToString(s.Sum(nil))
 }
 
 func calculateSalt(accountName string) string {

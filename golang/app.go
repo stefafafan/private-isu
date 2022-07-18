75eba4934c56249b764d8397eaf0f840416569d6
Stefan Utamaru <stefafafan@hatena.ne.jp>
2022-07-18 21:16:06 +0900

    pprof使いたいので8080ポートで再びみれるように

--- golang/app.go
+++ golang/app.go
@@ -9,7 +9,6 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -911,16 +910,17 @@ func main() {
 		http.FileServer(http.Dir("../public")).ServeHTTP(w, r)
 	})
 
-	socket_file := "/tmp/go.sock"
-	os.Remove(socket_file)
-	l, err := net.Listen("unix", socket_file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = os.Chmod(socket_file, 0777)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", r))
+	// socket_file := "/tmp/go.sock"
+	// os.Remove(socket_file)
+	// l, err := net.Listen("unix", socket_file)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err = os.Chmod(socket_file, 0777)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	log.Fatal(http.Serve(l, r))
+	// log.Fatal(http.Serve(l, r))
 }
